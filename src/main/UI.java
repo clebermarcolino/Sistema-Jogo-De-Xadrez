7f2725f5bed7adfaf2461b99e480cb5312d04bcc
cleber.marcolino <clebermarcolinogba@gmail.com>
2024-09-30 12:35:54 -0300

    Fiz um metódo na classe UI chamado exibirLogo() que tem como função exibir a frase 'CHESS MATCH' em ASC2 logo quando a aplicação inicia

--- src/main/UI.java
+++ src/main/UI.java
@@ -94,13 +94,15 @@ public class UI {
     }
 
     public static void exibirLogo() {
-        System.out.println("   ____ _           _    __  __           _   _       _   _           _   ");
-        System.out.println("  / ___| |__   __ _| |_|  \\/  | __ _ ___ | |_| |__   | |_| |__   __ _| |_ ");
-        System.out.println(" | |   | '_ \\ / _` | __| |\\/| |/ _` / __|| __| '_ \\  | __| '_ \\ / _` | __|");
-        System.out.println(" | |___| | | | (_| | |_| |  | | (_| \\__ \\| |_| | | | | |_| | | | (_| | |_ ");
-        System.out.println("  \\____|_| |_|\\__,_|\\__|_|  |_|\\__,_|___/ \\__|_| |_|  \\__|_| |_|\\__,_|\\__|");
+        System.out.println("   ____ _           _   __  __           _   _           _   _           _   ");
+        System.out.println("  / ___| |__   __ _| |_|  \\/  | __ _ ___ | |_| |__   __ _| |_| |__   __ _| |_ ");
+        System.out.println(" | |   | '_ \\ / _` | __| |\\/| |/ _` / __|| __| '_ \\ / _` | __| '_ \\ / _` | __|");
+        System.out.println(" | |___| | | | (_| | |_| |  | | (_| \\__ \\| |_| | | | (_| | |_| | | | (_| | |_ ");
+        System.out.println("  \\____|_| |_|\\__,_|\\__|_|  |_|\\__,_|___/ \\__|_| |_|\\__,_|\\__|_| |_|\\__,_|\\__|");
         System.out.println();
     }
+
+
 
 
     private static void mostrarPeca(PecaXadrez peca, boolean background) {
